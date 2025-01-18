8de96af8d90277da7691f5545c6c5d47fff99391
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-01-18 13:53:17 -0300

    inicio do JS do topo

--- JS/topo.js
+++ JS/topo.js
@@ -1,2 +1,26 @@
 const btn = document.querySelector('#btn');
 
+btn.addEventListener("click", function(){
+    const divOpcoesPersonagens = document.createElement('div');
+    divOpcoesPersonagens.classList.add('box_link_personagens')
+
+    const linkKratos = document.createElement('a');
+    linkKratos.setAttribute("href", "#kratos");
+    linkKratos.innerText = "Kratos";
+
+    const linkJoel = document.createElement('a');
+    linkJoel.setAttribute("href", "#joel");
+    linkJoel.innerText = "Joel";
+
+    const linkDeacon = document.createElement('a');
+    linkDeacon.setAttribute("href", "#deacon")
+    linkDeacon.innerText = "Deacon st.John"
+
+    const linkArthur = document.createElement('a');
+    linkArthur.setAttribute("href", "#arthur")
+    linkArthur.innerText = "Arthur Morgan"
+
+    divOpcoesPersonagens.append(linkKratos, linkJoel, linkDeacon, linkArthur);
+
+    btn.appendChild(divOpcoesPersonagens);
+})
