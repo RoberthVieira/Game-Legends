f3280c00f6eb9d1bcd3925772a15ad69a12f50f0
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-01-25 02:07:33 -0300

    criação de mais uma seção

--- JS/btn.js
+++ JS/btn.js
@@ -1,5 +1,7 @@
 const btnSaberMais =  document.querySelector('.btn_saber_mais');
 const clicado = document.querySelector('.esconder');
+const btnJoel = document.querySelector('.btn_joel');
+const click = document.querySelector('.escondido');
 
 btnSaberMais.addEventListener("click", () => {
     
@@ -9,5 +11,15 @@ btnSaberMais.addEventListener("click", () => {
         btnSaberMais.innerHTML = 'Clique aqui para saber mais';
     } else {
         btnSaberMais.innerHTML = 'Fechar';
+    }
+})
+
+btnJoel.addEventListener('click', () => {
+    const estaEscondido = click.classList.toggle('escondido');
+
+    if(estaEscondido){
+        btnJoel.innerHTML = 'Clique aqui para saber mais';
+    } else {
+        btnJoel.innerHTML = 'Fechar';
     }
 })
