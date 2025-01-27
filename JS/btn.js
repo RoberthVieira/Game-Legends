ff7e5f330cac9db606850d5c94d4dfbe621d2fb1
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-01-26 23:17:00 -0300

    Fim sessão terceiro personagem

--- JS/btn.js
+++ JS/btn.js
@@ -1,7 +1,10 @@
 const btnSaberMais =  document.querySelector('.btn_saber_mais');
 const clicado = document.querySelector('.esconder');
 const btnJoel = document.querySelector('.btn_joel');
-const click = document.querySelector('.escondido');
+const clicado2 = document.querySelector('.escondido');
+const btnDeacon = document.querySelector('.btn_deacon');
+const clicado3 = document.querySelector('.esconde');
+
 
 btnSaberMais.addEventListener("click", () => {
     
@@ -12,14 +15,24 @@ btnSaberMais.addEventListener("click", () => {
     } else {
         btnSaberMais.innerHTML = 'Fechar';
     }
-})
+});
 
 btnJoel.addEventListener('click', () => {
-    const estaEscondido = click.classList.toggle('escondido');
+    const estaEscondido = clicado2.classList.toggle('escondido');
 
     if(estaEscondido){
         btnJoel.innerHTML = 'Clique aqui para saber mais';
     } else {
         btnJoel.innerHTML = 'Fechar';
+    }
+});
+
+btnDeacon.addEventListener("click", () => {
+    const estaEscondido = clicado3.classList.toggle('esconde');
+
+    if(estaEscondido){
+        btnDeacon.innerHTML = 'Clique aqui para saber mais';
+    } else {
+        btnDeacon.innerHTML = 'Fechar';
     }
 })
